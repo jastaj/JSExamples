9b990c7a8b931cd473c7154dea07379a320f9538
Dharmpreet Singh <dharmhp78@gmail.com>
2023-08-10 22:47:41 +0530

    Challange 1 done

---
+++ JScriptExamples/Dharampreet/operation2.js
@@ -0,0 +1,5 @@
+var num1=parseInt(prompt('Enter first number : '))
+var num2=parseInt(prompt('Enter Second number : '))
+// var oper=prompt('Enter operation 1 for Additon, 2 for Subtraction, 3 for Multiplication, 4 for Division ');
+if(oper==+)
+console.log(`${num1} ${oper} ${num2} `);
