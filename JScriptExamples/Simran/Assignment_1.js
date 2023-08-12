66e225e1ea145243469d7c42df5a3600aec9b7c9
Simran Kaur <simran6373kaur@gmail.com>
2023-08-11 22:44:41 -0700

    Js file

--- JScriptExamples/Simran/Assignment_1.js
+++ JScriptExamples/Simran/Assignment_1.js
@@ -25,3 +25,20 @@ var john_BMI=john_mass/(john_height*john_height);
 
 var markHigherBMI = mark_BMI > john_BMI;
 console.log(markHigherBMI);
+
+
+
+//Data 2:Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76m tall.
+
+//for Mark
+var mark_mass=95;
+var mark_height=1.88;
+var mark_BMI=mark_mass/(mark_height*mark_height);
+
+//for John
+var john_mass=85;
+var john_height=1.76;
+var john_BMI=john_mass/(john_height*john_height);
+
+var markHigherBMI = mark_BMI > john_BMI;
+console.log(markHigherBMI);
