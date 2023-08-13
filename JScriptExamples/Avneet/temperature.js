f42c246bab207c90032c416eae76c8ba9036364d
Avneet kaur <avneetkaur3138@gmail.com>
2023-08-13 19:01:25 +0530

    file of converting temperatures- avneet

---
+++ JScriptExamples/Avneet/temperature.js
@@ -0,0 +1,11 @@
+// temperature changes celsius to Fahrenheit
+
+var celsius=parseInt(prompt('Enter a celsius value:'));
+fahrenheit=(celsius * 9/5)+ 32;
+console.log("The value of the temperature in Fahrenheit is "+fahrenheit);
+
+// Fahrenhiet into celcius
+
+var fahrenheit=parseInt(prompt('Enter a Fahrenheit value:'))
+celsius=(fahrenheit - 32) * 5/9;
+console.log("The value of the temperature in Fahrenheit is "+celsius);
