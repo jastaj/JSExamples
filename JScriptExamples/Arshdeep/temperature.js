c9f083f45e7d8787a9ed9a0ab471284912087d91
Arsh <arshk7696@gmail.com>
2023-08-13 22:41:02 +0530

    The fifth file

--- JScriptExamples/Arshdeep/temperature.js
+++ JScriptExamples/Arshdeep/temperature.js
@@ -1,7 +1,12 @@
-var num=parseInt("Enter a celsius value: ");
+// changes celsius to Fahrenheit
 
-var fahrenheit = (cel * 1.8) + 32
+var celsius=parseFloat(prompt('Enter a celsius value:'));
+fahrenheit=(celsius * 9/5)+ 32;
+document.write("The value of the temperature in Fahrenheit is :"+fahrenheit);
 
+// Fahrenhiet into celcius
 
-
+var fahrenheit=parseInt(prompt('Enter a Fahrenheit value:'));
+celsius=(fahrenheit - 32) * 5/9;
+document.write("The value of the temperature in Fahrenheit is :"+celsius);
 
