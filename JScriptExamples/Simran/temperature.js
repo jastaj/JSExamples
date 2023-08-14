6a1d8894b4e9f2b085dd30959df9172b058ff358
Simran Kaur <simran6373kaur@gmail.com>
2023-08-13 23:11:58 -0700

    Temperature conversion

---
+++ JScriptExamples/Simran/temperature.js
@@ -0,0 +1,9 @@
+// Convert Celsius into Fahrenheit
+var celsius=parseFloat(prompt('Enter a value for conversion :'));
+var fahrenheit=(celsius*9/5)+32;
+document.write('The conversion of celsius temperature into fahrenheit is '+fahrenheit+'<br>');
+
+// Convert Fahrenheit into celsius
+var fahren_heit=parseInt(prompt('Enter a value for conversion :'));
+var cel_sius=(fahren_heit-32)*5/9 ;
+document.write('The conversion of fahrenheit temperature in celsius is '+cel_sius);
