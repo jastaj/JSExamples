fa6a58559dfb8fe16f7c015afab7984ef96811e0
Simran Kaur <simran6373kaur@gmail.com>
2023-08-13 21:14:58 -0700

    again swapping

--- JScriptExamples/Simran/swapping.js
+++ JScriptExamples/Simran/swapping.js
@@ -1,21 +1,21 @@
 // Using document.write
-var num1=prompt('Enter first number:');
+// var num1=prompt('Enter first number:');
 
-var num2=prompt('Enter second number:');
+// var num2=prompt('Enter second number:');
 
-document.write('Values before swapping'+'<br>');
-document.write('*******************************'+'<br>');
-document.write('First number is '+num1+'<br>');
-document.write('Second number is '+num2+'<br>'+'<br>');
+// document.write('Values before swapping'+'<br>');
+// document.write('*******************************'+'<br>');
+// document.write('First number is '+num1+'<br>');
+// document.write('Second number is '+num2+'<br>'+'<br>');
 
-var temp=num2;
-num2=num1;
-num1=temp;
+// var temp=num2;
+// num2=num1;
+// num1=temp;
 
-document.write('Values after Swapping'+'<br>');
-document.write('*******************************'+'<br>');
-document.write('First number is '+num1+'<br>');
-document.write('Second number is '+num2+'<br>');
+// document.write('Values after Swapping'+'<br>');
+// document.write('*******************************'+'<br>');
+// document.write('First number is '+num1+'<br>');
+// document.write('Second number is '+num2+'<br>');
 
 // Using console.log
 
@@ -23,16 +23,16 @@ var number1=prompt('Enter first number:');
 
 var number2=prompt('Enter second number:');
 
-console.log('\nValues before swapping');
-console.log('\n*******************************');
-console.log('\nFirst number is '+number1);
-console.log('\nSecond number is '+number2);
+console.log('Values before swapping');
+console.log('*******************************');
+console.log('First number is '+number1);
+console.log('Second number is '+number2);
 
 var tem_p=number2;
 number2=number1;
 number1=tem_p;
 
 console.log('\nValues after Swapping');
-console.log('\n*******************************');
-console.log('\nFirst number is '+number1);
-console.log('\nSecond number is '+number2);
+console.log('*******************************');
+console.log('First number is '+number1);
+console.log('Second number is '+number2);
