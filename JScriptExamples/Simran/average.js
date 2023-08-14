c03d50f6c4822a91175182efce9cdc6bd83d509e
Simran Kaur <simran6373kaur@gmail.com>
2023-08-13 21:39:39 -0700

    Calculate average

---
+++ JScriptExamples/Simran/average.js
@@ -0,0 +1,10 @@
+// Calculate average
+
+var num1=parseInt(prompt('Enter first number:'));
+var num2=parseInt(prompt('Enter second number:'));
+var num3=parseInt(prompt('Enter third number:'));
+var num4=parseInt(prompt('Enter fourth number:'));
+
+avg=(num1+num2+num3+num4)/4;
+
+document.write('Average of entered numbers is '+avg);
