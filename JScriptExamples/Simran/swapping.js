87f669b299d87fa89fdc7841e43ef5b06554020b
Simran Kaur <simran6373kaur@gmail.com>
2023-08-13 19:27:10 -0700

    Performed swapping

---
+++ JScriptExamples/Simran/swapping.js
@@ -0,0 +1,35 @@
+// var num1=prompt('Enter first number:');
+
+// var num2=prompt('Enter second number:');
+
+// document.write('\nValues before swapping');
+// document.write('\n*******************************');
+// document.write('\nFirst number is '+num1);
+// document.write('\nSecond number is '+num2);
+
+// var temp=num2;
+// num2=num1;
+// num1=temp;
+
+// document.write('\nValues after Swapping');
+// document.write('\n*******************************');
+// document.write('\nFirst number is '+num1);
+// document.write('\nSecond number is '+num2);
+
+var number1=prompt('Enter first number:');
+
+var number2=prompt('Enter second number:');
+
+console.log('\nValues before swapping');
+console.log('\n*******************************');
+console.log('\nFirst number is '+number1);
+console.log('\nSecond number is '+number2);
+
+var tem_p=number2;
+number2=number1;
+number1=tem_p;
+
+console.log('\nValues after Swapping');
+console.log('\n*******************************');
+console.log('\nFirst number is '+number1);
+console.log('\nSecond number is '+number2);
