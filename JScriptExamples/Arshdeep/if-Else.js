afa105cb6a2d8ec43099684c7fb82de251fea826
Arsh <arshk7696@gmail.com>
2023-08-15 16:10:22 +0530

    If-Statements

---
+++ JScriptExamples/Arshdeep/if-Else.js
@@ -0,0 +1,45 @@
+//Greatest number
+
+var num1=parseInt(prompt('Enter first number'));
+var num2=parseInt(prompt('Enter second number'));
+
+if(num1>num2)
+{
+    document.write  ('First number is greater');
+}
+else
+{
+    document.write('Second number is greater');
+}
+
+//Positive or Negative number
+
+var num=parseInt(prompt('Enter a number'));
+
+if(num>0)
+{
+    document.write('Number is positive');
+}
+else
+{
+    document.write('Number is negative');
+}
+
+
+//Number odd or even
+
+var num=parseInt(prompt('Enter a number'));
+
+if(num%2==0)
+{
+    document.write('Number is even');
+}
+else
+{
+    document.write('Number is odd');
+}
+
+
+
+
+
