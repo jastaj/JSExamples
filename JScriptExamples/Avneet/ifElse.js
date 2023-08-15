966ac1f52f919da0b55645c221eb18c68f373e6b
Avneet kaur <avneetkaur3138@gmail.com>
2023-08-15 16:22:01 +0530

    Performed if else

---
+++ JScriptExamples/Avneet/ifElse.js
@@ -0,0 +1,10 @@
+var num1=parseInt(prompt('Enter first number'));
+var num2=parseInt(prompt('Enter second number'));
+if(num1>num2)
+{
+    console.log('First number is greater');
+}
+else
+{
+    console.log('Second number is greater');
+}
