f87bb6be8c9ff12487840f9560202cb2cb4c0d52
Simran Kaur <simran6373kaur@gmail.com>
2023-08-16 15:04:30 -0700

    ifElse

---
+++ JScriptExamples/Simran/ifElse.js
@@ -0,0 +1,42 @@
+// To find a greatest number between two numbers.
+
+var num1=parseInt(prompt('Enter a first number:'));
+var num2=parseInt(prompt('Enter a second number:'));
+
+if(num1>num2)
+{
+    document.write('First number is greater.');
+}
+else
+{
+    document.write('Second number is greater.');
+}
+
+
+// To find the number is positive or negative.
+
+var number=parseInt(prompt('Enter a number:'));
+
+if(number>0)
+{
+    document.write('Number is positive.');
+}
+else
+{
+    document.write('Number is negative.')
+}
+
+
+// To find the number is Even or Odd.
+
+var num_ber=parseInt(prompt('Enter a first number:'));
+
+
+if(num_ber%2==0)
+{
+    document.write('Number is Even.');
+}
+else
+{
+    document.write('Number is Odd.');
+}
