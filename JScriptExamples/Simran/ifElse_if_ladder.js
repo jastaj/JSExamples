d916f0d2c66dd789e6002117adbb113772fd5ddb
Simran Kaur <simran6373kaur@gmail.com>
2023-08-16 22:44:08 -0700

    using ifElse if ladder

---
+++ JScriptExamples/Simran/ifElse_if_ladder.js
@@ -0,0 +1,36 @@
+var ch=prompt('Enter your choice from 1-7');
+
+if(ch==1)
+{
+    document.write('Sunday');
+}
+
+if(ch==2)
+{
+    document.write('Monday');
+}
+
+if(ch==3)
+{
+    document.write('Tuesday');
+}
+
+if(ch==4)
+{
+    document.write('Wednesday');
+}
+
+if(ch==5)
+{
+    document.write('Thursday');
+}
+
+if(ch==6)
+{
+    document.write('Friday');
+}
+
+if(ch==7)
+{
+    document.write('Saturday');
+}
