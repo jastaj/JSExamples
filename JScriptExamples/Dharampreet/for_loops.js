7cfabf2a36dbbf22bbeea65a607e47ef9d8a18aa
Dharmpreet Singh <dharmhp78@gmail.com>
2023-08-22 22:37:46 +0530

    For loop Examples

---
+++ JScriptExamples/Dharampreet/for_loops.js
@@ -0,0 +1,46 @@
+// -----Factorial-----
+// var n=parseInt(prompt("Enter number to find factorial :"));
+// var fact=1;
+// var sum=0;
+// for(i=1;i<=n;i++)
+// {
+//     fact=fact*i;
+//     sum=sum+(i/fact);
+// }
+// console.log("Factorial = "+fact);
+// console.log("Sum = "+sum);
+
+// -----Fibonacci Series-----
+// var n=parseInt(prompt("Enter number to find Fibonacci Series :"));
+// var pre=0;
+// var curr=1;
+// var next;
+// console.log(pre);
+// console.log(curr);
+// for(i=0;i<=n;i++)
+// {
+//     next=pre+curr;
+//     pre=curr;
+//     curr=next;
+//     console.log(next);
+// }
+
+// -----Prime Number-----
+var n=parseInt(prompt("Enter number to find it's prime  or not :"));
+var flag=0;
+for(i=2;i<n;i++)
+{
+    if(n%i==0)
+    {
+        flag=1;
+        break;
+    }
+}
+if(flag==0)
+{
+    console.log("Number is prime ");
+}
+else
+{
+    console.log("Number is not prime ");
+}
---
+++ JScriptExamples/Dharampreet/test.js
@@ -0,0 +1,16 @@
+// var n=prompt("Enter maximum:");
+// let i=n;
+// while(i>=1)
+// {
+//     console.log(i);
+//     i--;
+// }
+
+// -----Table-----
+var n=prompt("Enter number:");
+let i=1;
+while(i<=10)
+{
+    console.log(n +'x'+i+'='+n*i);
+    i++;
+}
