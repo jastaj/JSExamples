9da2b3ca9a0ee3cd028324d3836f7b7eb0770af1
Jastaj <jastaj.grewal@gmail.com>
2023-08-23 16:17:16 +0530

    Nested For Loop

---
+++ JScriptExamples/Jastaj/nestedForLoop.js
@@ -0,0 +1,44 @@
+// var n=parseInt(prompt('How many rows'));
+
+// for(var i=1;i<=n;i++)
+// {
+//     var st="";
+//     for(j=1;j<=i;j++)
+//     {
+//         st=st+"*"+" ";
+//     }
+//     console.log(st);
+// }
+
+//Reverse
+
+// var n=parseInt(prompt('How many rows'));
+
+// for(var i=n;i>=1;i--)
+// {
+//     var st="";
+//     for(j=1;j<=i;j++)
+//     {
+//         st=st+"*"+" ";
+//     }
+//     console.log(st);
+// }
+
+//Example 3
+//1
+//2 2
+//3 3 3
+//4 4 4 4
+//5 5 5 5 5
+
+var n=parseInt(prompt('How many rows'));
+
+for(var i=1;i<=n;i++)
+{
+    var st="";
+    for(j=1;j<=i;j++)
+    {
+        st=st+i+" ";
+    }
+    console.log(st);
+}
