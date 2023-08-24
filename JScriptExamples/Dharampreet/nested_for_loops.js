a2865b0992c5781d3150dc84899bf6ca25a8b3f6
Dharmpreet Singh <dharmhp78@gmail.com>
2023-08-24 23:35:38 +0530

    Diamond and rectangle

--- JScriptExamples/Dharampreet/nested_for_loops.js
+++ JScriptExamples/Dharampreet/nested_for_loops.js
@@ -40,19 +40,76 @@
 // }
 
 // -----Pyramid Pattren-----
-var n=parseInt(prompt("Enter number of lines to triangle :"));
-for(i=1; i<=n; i++)
+// var n=parseInt(prompt("Enter number of lines to triangle :"));
+// for(i=1; i<=n; i++)
+// {
+//     var line="";
+//     for(k=n-1; k>=i; k--)
+//     {
+//         line=line+" ";
+
+//     }
+
+//     for(j=1; j<=i; j++)
+//     {
+//         line=line+j+" ";
+//     }
+//     console.log(line);
+// }
+
+// -----Diamond Pattren-----
+// var n=parseInt(prompt("Enter number of lines to pattren :"));
+// for(i=1; i<=n; i++)
+// {
+//     var line="";
+//     for(k=n-1; k>=i; k--)
+//     {
+//         line=line+" ";
+
+//     }
+
+//     for(j=1; j<=i; j++)
+//     {
+//         line=line+"*"+" ";
+//     }
+//     console.log(line);
+// }
+// for(i=n-1; i>=1; i--)
+// {
+//     var line="";
+//     for(k=i; k<n; k++)
+//     {
+//         line=line+" ";
+
+//     }
+
+//     for(j=1; j<=i; j++)
+//     {
+//         line=line+"*"+" ";
+//     }
+//     console.log(line);
+// }
+
+//-----Empty rectangle-----
+var r=parseInt(prompt("Enter number of rows to rectangle :"));
+var c=parseInt(prompt("Enter number of columns to rectangle :"));
+var i,j;
+for(i=1 ; i<=r; i++)
 {
     var line="";
-    for(k=n-1; k>=i; k--)
+    for(j=1; j<=c; j++)
     {
-        line=line+" ";
-
+        if(i==1||i==r||j==1||j==c)
+        {
+            // line=line+"*";
+            document.write("*");
+        }
+        else
+        {
+            // line=line+i;
+            document.write("&nbsp;&nbsp;");
+        }
     }
-
-    for(j=1; j<=i; j++)
-    {
-        line=line+j+" ";
-    }
-    console.log(line);
+    // console.log(line);
+    document.write("<br>");
 }
