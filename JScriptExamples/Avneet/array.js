e01e92dde05dc36e5fbd0880ae630fd85b62abd6
Avneet Kaur <avneetkaur3138@gmail.com>
2023-08-25 10:24:39 -0700

    Array Examples - avneet

---
+++ JScriptExamples/Avneet/array.js
@@ -0,0 +1,11 @@
+let myArray=[10,20,30,40,50];
+
+var sum=0;
+for(var i=0;i<myArray.length;i++)
+{
+    sum=sum+myArray[i];
+    var mean=sum/myArray.length;
+}
+
+console.log('Sum of elements ='+sum);
+console.log('Mean ='+mean);
