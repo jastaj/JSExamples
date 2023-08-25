32365ae30217c18d7f9ccda841158c5cad9c7914
Jastaj <jastaj.grewal@gmail.com>
2023-08-25 21:38:46 +0530

    Array and For Loop Examples

--- JScriptExamples/Jastaj/prime.js
+++ JScriptExamples/Jastaj/prime.js
@@ -1,19 +1,44 @@
-var n=parseInt(prompt('Enter any number ='));
-var flag=0;
+// var n=parseInt(prompt('Enter any number ='));
+// var flag=0;
 
-for(i=2 ;i<n;i++)
+// for(i=2 ;i<n;i++)
+// {
+//     if(n%i==0)
+//     {
+//         flag=1;
+//         break;
+//     }
+// }
+
+// if(flag==0)
+// {
+//     console.log('Number is prime');
+// }
+// else{
+//     console.log('Number is not prime');
+// }
+
+
+var n=parseInt(prompt('Enter first number of the range='));
+var m=parseInt(prompt('Enter last number of the range='));
+
+
+for(var j=n;j<=m;j++)
 {
-    if(n%i==0)
+    var flag=0;
+    for(i=2 ;i<j;i++)
     {
-        flag=1;
-        break;
+        if(j%i==0)
+        {
+            flag=1;
+            
+        }
     }
-}
 
-if(flag==0)
-{
-    console.log('Number is prime');
-}
-else{
-    console.log('Number is not prime');
+    if(flag==0)
+    {
+        console.log(j);
+    }
+    
+
 }
