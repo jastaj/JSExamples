95885fd5620504640ff8624ed04d73fb5d02debc
Gurwinder <gurwinder771993@gmail.com>
2023-08-28 16:45:51 +0530

    Reverse order

---
+++ JScriptExamples/Dharampreet/reverse_order.js
@@ -0,0 +1,11 @@
+
+
+var n=parseInt(prompt('Enter maximum limit'));
+var i=n;
+while(i>=1)
+{
+    console.log(i);
+    i--;
+    
+
+}
---
+++ JScriptExamples/Gurwinder/tip.js
@@ -0,0 +1,11 @@
+ var x=50 ;
+ var x=300 ;
+var x=parseInt(prompt("Enter the value:"));
+if( x>=50 && x<=300) 
+{
+    console.log("You are eligible for 15% tip")
+}
+else
+{
+console.log("You are eligible for tip");
+}
