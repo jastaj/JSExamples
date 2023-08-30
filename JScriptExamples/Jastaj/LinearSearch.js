59953e50d41713bfe732026f0aa9df3aa3a91d45
Jastaj <jastaj.grewal@gmail.com>
2023-08-30 20:32:43 +0530

    Bubble Sort

--- JScriptExamples/Jastaj/LinearSearch.js
+++ JScriptExamples/Jastaj/LinearSearch.js
@@ -1,20 +1,59 @@
+// var myArray=[55,7,99,8,66,2,98];
+// var element=parseInt(prompt('Enter the element to be searched'));
+// var loc=-1;
+// for(var i=0;i<myArray.length;i++)
+// {
+//     if(element==myArray[i])
+//     {
+//         loc=i;
+//         break;
+//     }
+// }
+// if(loc==-1)
+// {
+//     console.log('element not found');    
+    
+// }
+// else
+// {
+//     console.log('Element found at location = '+loc);    
+// }
 var myArray=[55,7,99,8,66,2,98];
-var element=parseInt(prompt('Enter the element to be searched'));
-var loc=-1;
+MAX=myArray[0];
+//myArray.sort();
+
 for(var i=0;i<myArray.length;i++)
 {
-    if(element==myArray[i])
+    if(MAX<myArray[i])
     {
-        loc=i;
-        break;
+        MAX=myArray[i];
     }
 }
-if(loc==-1)
+console.log(MAX);
+SMAX=myArray[0];
+for(var i=0;i<myArray.length;i++)
 {
-    console.log('element not found');    
-    
+    if(myArray[i]!=MAX)
+    {
+        if(SMAX<myArray[i])
+        {
+            SMAX=myArray[i];
+        }
+    }
 }
-else
-{
-    console.log('Element found at location = '+loc);    
-}
+
+console.log(SMAX);
+// var myArray=[5,14,28,66,52];
+
+// for(var k=0;k<myArray.length;k++)
+// {
+//     for(var i=k;i<myArray.length;i++)
+//     {
+//         var st=" ";
+//         for(var j=k;j<=i;j++)
+//         {
+//             st=st+myArray[j]+" ";
+//         }
+//         console.log(st)
+//     }
+// }
