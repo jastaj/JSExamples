560aaa205e63802eff33ae104e4320e1e848b38a
Arsh <arshk7696@gmail.com>
2023-08-30 21:43:24 +0530

    File of array

---
+++ JScriptExamples/Arshdeep/bubble-sort.js
@@ -0,0 +1,18 @@
+var arr=[100,7,99,8,66,2,98,1];
+for(i=0;i<arr.length;i++)   //Passes
+{
+    for(j=0;j<arr.length-i;j++)
+    {
+        if(arr[j]>arr[j+1])
+        {
+            temp=arr[j+1];
+            arr[j+1]=arr[j];
+            arr[j]=temp;
+        }
+    }
+}
+
+for(i=0;i<arr.length;i++)
+{
+    console.log(arr[i]);
+} 
---
+++ JScriptExamples/Arshdeep/maxi-min.js
@@ -0,0 +1,26 @@
+// //to find maximum value
+
+var myArray=[5,77,45,80,16,52,68];
+var MAX=myArray[0];
+for(i=0; i<myArray.length; i++)
+{
+    if (MAX<myArray[i])
+    {
+        MAX=myArray[i];
+    }
+}
+console.log(MAX);
+
+
+//to find minimum value
+
+var myArray=[4,57,60,10,16,72,98];
+var MIN=myArray[0];
+for(i=0; i<myArray.length; i++)
+{
+    if (MIN>myArray[i])
+    {
+        MIN=myArray[i];
+    }
+}
+console.log(MIN);
